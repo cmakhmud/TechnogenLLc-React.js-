67f1fa2318e6ab509e45f428f96c6e18dd4cd4a9
cmakhmud <98968434+cmakhmud@users.noreply.github.com>
2022-05-07 21:27:33 +0400

    Add files via upload

---
+++ src/Blog/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section23 from '../Section23/Section23'
+import Section71 from '../Section71/Section71'
+import Section8 from '../Section8/Section8'
+function Blog() {
+  return (
+    <div>
+      <Section23 />
+      <Section71 />
+    </div>
+  )
+}
+
+export default Blog
